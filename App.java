627ff1d7ebfc8a1d1f6767d480984cd199cc40a8
EnzoMN26 <enzodrao@gmail.com>
2023-03-22 18:03:52 -0300

    alteracao ex1.4

--- App.java
+++ App.java
@@ -4,5 +4,6 @@ public class App {
         Singleton instancia = Singleton.getInstance();
         instancia.addElement(1, 3, 5, 4, 2, 6, 7);
         System.out.println(instancia.getElement(4)); 
+        System.out.println(instancia.getElements());
     }
 }
